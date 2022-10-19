4db831dbc943dc8e4a0c2983f65ff04200ceff60
goederts <goederts@msoe.edu>
2022-10-19 12:53:23 -0500

    Copy non html files

--- dev/compile.py
+++ dev/compile.py
@@ -1,5 +1,6 @@
 import os
 import re
+import shutil
 
 def find_files(src):
     files = []
@@ -12,35 +13,41 @@ def find_files(src):
 
 
 def combine(template, file_loc: str, variables):
-    f = open('dev/src/' + file_loc)
-    file = f.read()
-    f.close()
 
-    i = file.index("$ENDHEADER$")
-    header = file[:i]
-    content = file[i+11:].strip()
-
-    values = {}
-    for line in re.findall("%[a-z]+% = .+\n", header):
-        values[line[:line.index(' ')]] = line[line.index('=') + 2:-1]
-
-    o = template
-    for v in variables:
-        if v in values:
-            o = re.sub(v, values[v], o)
-        else:
-            if v != "%content%":
-                o = re.sub(v, "", o)
-
-
-    o = re.sub('%content%', content, o)
+    # Create the folder that contains the file if it doesn't exist
     if "/" in file_loc:
         directory = file_loc[:file_loc.rindex("/")]
         if not os.path.exists(directory):
             os.mkdir(directory)
-    f2 = open(file_loc, 'w')
-    f2.write(o)
-    f2.close()
+
+    # Check if it's a html doc, if not just copy the file to the destination
+    if file_loc.endswith('.html'):
+        f = open('dev/src/' + file_loc)
+        file = f.read()
+        f.close()
+
+        i = file.index("$ENDHEADER$")
+        header = file[:i]
+        content = file[i+11:].strip()
+
+        values = {}
+        for line in re.findall("%[a-z]+% = .+\n", header):
+            values[line[:line.index(' ')]] = line[line.index('=') + 2:-1]
+
+        o = template
+        for v in variables:
+            if v in values:
+                o = re.sub(v, values[v], o)
+            else:
+                if v != "%content%":
+                    o = re.sub(v, "", o)
+        o = re.sub('%content%', content, o)
+        f2 = open(file_loc, 'w')
+        f2.write(o)
+        f2.close()
+
+    else:
+        shutil.copy("dev/src/" + file_loc, file_loc)
 
 
 def main():
